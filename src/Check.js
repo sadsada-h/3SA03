ce3ea9a477aba14fddfdffe4a33d6c5cb7959a7e
sadsada-h <sadsadahandloon@gmail.com>
2019-09-23 22:24:49 +0700

    commit 7

---
+++ src/Check.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Check.css';
+import Event from './Event';
+import App from './App'
+class Check extends React.Component {
+    render() {
+        let count_end = this.props.check_count > 10 ? 'area2' : '';
+        let count_ends = this.props.check_count > 10 ? "Game Over" : "Counter : " + this.props.check_count;
+        let test = `area ${count_end}`
+        return (
+            <div>
+                <div className={test}>
+                    <h1 className={test}>{this.count_end}{count_ends}</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Check;
